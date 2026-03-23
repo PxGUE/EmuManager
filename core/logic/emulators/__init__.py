ad4ee842d37a6c527fc9c41045a9c5313d4612e3
PxGUE <itmio.bel@gmail.com>
2026-03-23 12:18:06 -0500

    chore: limpieza total para la reescritura v2

--- core/logic/emulators/__init__.py
+++
@@ -1,3 +0,0 @@
-﻿from .manager import EmuladorManager
-
-__all__ = ["EmuladorManager"]
--- core/security/_secrets.py
+++
@@ -1,6 +0,0 @@
-# --- EmuManager - Secretos de Desarrollador (NO SUBIR A REPOSITORIO) ---
-# Este archivo es ideal para Nuitka; tus claves se compilarán en C++ 
-# y serán muy difíciles de extraer del ejecutable final.
-
-SS_DEV_ID = "paidex"
-SS_DEV_PASSWORD = "zg9u2oGAY1z"
